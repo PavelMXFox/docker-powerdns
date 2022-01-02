f0d6bf542e89ebdea2b715d1fe3f659648f9e468
Pavel Dmitriev <90725813+PavelMXFox@users.noreply.github.com>
2022-01-02 14:06:24 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,59 +37,69 @@ docker_setup_env() {
     file_env 'MYSQL_PORT' $MYSQL_DEFAULT_PORT
 }
 
-docker_setup_env
+[ ! -d "/etc/pdns/conf.d" ] && mkdir /etc/pdns/conf.d
 
-# --help, --version
-[ "$1" = "--help" ] || [ "$1" = "--version" ] && exec pdns_server $1
-# treat everything except -- as exec cmd
-[ "${1:0:2}" != "--" ] && exec "$@"
+if [ ! -f "/etc/pdns/pdns.conf" ]
+then 
+  cp -f /etc/pdns_template/* /etc/pdns/
+  docker_setup_env
 
-if $MYSQL_AUTOCONF ; then
-  # Set MySQL Credentials in pdns.conf
-  sed -r -i "s/^[# ]*gmysql-host=.*/gmysql-host=${MYSQL_HOST}/g" /etc/pdns/pdns.conf
-  sed -r -i "s/^[# ]*gmysql-port=.*/gmysql-port=${MYSQL_PORT}/g" /etc/pdns/pdns.conf
-  sed -r -i "s/^[# ]*gmysql-user=.*/gmysql-user=${MYSQL_USER}/g" /etc/pdns/pdns.conf
-  sed -r -i "s/^[# ]*gmysql-password=.*/gmysql-password=${MYSQL_PASS}/g" /etc/pdns/pdns.conf
-  sed -r -i "s/^[# ]*gmysql-dbname=.*/gmysql-dbname=${MYSQL_DB}/g" /etc/pdns/pdns.conf
-  sed -r -i "s/^[# ]*gmysql-dnssec=.*/gmysql-dnssec=${MYSQL_DNSSEC}/g" /etc/pdns/pdns.conf
+  # --help, --version
+  [ "$1" = "--help" ] || [ "$1" = "--version" ] && exec pdns_server $1
+  # treat everything except -- as exec cmd
+  [ "${1:0:2}" != "--" ] && exec "$@"
 
-  MYSQLCMD="mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} --port=${MYSQL_PORT} -r -N"
+  if $MYSQL_AUTOCONF ; then
+    # Set MySQL Credentials in pdns.conf
+    sed -r -i "s/^[# ]*gmysql-host=.*/gmysql-host=${MYSQL_HOST}/g" /etc/pdns/pdns.conf
+    sed -r -i "s/^[# ]*gmysql-port=.*/gmysql-port=${MYSQL_PORT}/g" /etc/pdns/pdns.conf
+    sed -r -i "s/^[# ]*gmysql-user=.*/gmysql-user=${MYSQL_USER}/g" /etc/pdns/pdns.conf
+    sed -r -i "s/^[# ]*gmysql-password=.*/gmysql-password=${MYSQL_PASS}/g" /etc/pdns/pdns.conf
+    sed -r -i "s/^[# ]*gmysql-dbname=.*/gmysql-dbname=${MYSQL_DB}/g" /etc/pdns/pdns.conf
+    sed -r -i "s/^[# ]*gmysql-dnssec=.*/gmysql-dnssec=${MYSQL_DNSSEC}/g" /etc/pdns/pdns.conf
+    sed -r -i "s/^[# ]*webserver-password=.*/webserver-password=${WEBSERVER_PASSWORD}/g" /etc/pdns/pdns.conf
+    sed -r -i "s/^[# ]*api-key=.*/api-key=${API_KEY}/g" /etc/pdns/pdns.conf
 
-  # wait for Database come ready
-  isDBup () {
-    echo "SHOW STATUS" | $MYSQLCMD 1>/dev/null
-    echo $?
-  }
+    MYSQLCMD="mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} --port=${MYSQL_PORT} -r -N"
 
-  RETRY=10
-  until [ `isDBup` -eq 0 ] || [ $RETRY -le 0 ] ; do
-    echo "Waiting for database to come up"
-    sleep 5
-    RETRY=$(expr $RETRY - 1)
-  done
-  if [ $RETRY -le 0 ]; then
-    >&2 echo Error: Could not connect to Database on $MYSQL_HOST:$MYSQL_PORT
-    exit 1
-  fi
+    # wait for Database come ready
+    isDBup () {
+      echo "SHOW STATUS" | $MYSQLCMD 1>/dev/null
+      echo $?
+    }
 
-  # init database if necessary
-  echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;" | $MYSQLCMD
-  MYSQLCMD="$MYSQLCMD $MYSQL_DB"
-
-  if [ "$(echo "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = \"$MYSQL_DB\";" | $MYSQLCMD)" -le 1 ]; then
-    echo Initializing Database
-    cat /etc/pdns/schema.sql | $MYSQLCMD
-
-    # Run custom mysql post-init sql scripts
-    if [ -d "/etc/pdns/mysql-postinit" ]; then
-      for SQLFILE in $(ls -1 /etc/pdns/mysql-postinit/*.sql | sort) ; do
-        echo Source $SQLFILE
-        cat $SQLFILE | $MYSQLCMD
-      done
+    RETRY=10
+    until [ `isDBup` -eq 0 ] || [ $RETRY -le 0 ] ; do
+      echo "Waiting for database to come up"
+      sleep 5
+      RETRY=$(expr $RETRY - 1)
+    done
+    if [ $RETRY -le 0 ]; then
+      >&2 echo Error: Could not connect to Database on $MYSQL_HOST:$MYSQL_PORT
+      exit 1
     fi
+
+    # init database if necessary
+    echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;" | $MYSQLCMD
+    MYSQLCMD="$MYSQLCMD $MYSQL_DB"
+
+    if [ "$(echo "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = \"$MYSQL_DB\";" | $MYSQLCMD)" -le 1 ]; then
+      echo Initializing Database
+      cat /etc/pdns/schema.sql | $MYSQLCMD
+
+      # Run custom mysql post-init sql scripts
+      if [ -d "/etc/pdns/mysql-postinit" ]; then
+        for SQLFILE in $(ls -1 /etc/pdns/mysql-postinit/*.sql | sort) ; do
+          echo Source $SQLFILE
+          cat $SQLFILE | $MYSQLCMD
+        done
+      fi
+    fi
+
+    unset -v MYSQL_PASS
   fi
 
-  unset -v MYSQL_PASS
+  echo "The file does not exist"; 
 fi
 
 # Run pdns server
